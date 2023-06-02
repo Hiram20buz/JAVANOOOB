5c8b2d2db90b09d28455b22f7a925809295f185f
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-01 19:01:44 -0700

    Create enums.java

---
+++ enums.java
@@ -0,0 +1,22 @@
+public class enums {
+    enum Rank {
+        SOLDIER,
+        SERGEANT,
+        CAPTAIN
+    }
+    public static void main(String[] args) {
+        Rank a = Rank.SOLDIER;
+                
+        switch(a) {
+            case SOLDIER:
+                System.out.println("Soldier says hi!");
+                break;
+            case SERGEANT:
+                System.out.println("Sergeant says Hello!");
+                break;
+            case CAPTAIN:
+                System.out.println("Captain says Welcome!");
+                break;
+        }
+    }
+}
