4bd2083284a55d0f1e8ac58079bd6c271e8615a2
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-01 18:48:50 -0700

    Create casting.java

---
+++ casting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class casting {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       char a = read.next().charAt(0);
+       int b = (int) a;
+       System.out.println(b);
+       }
+}
