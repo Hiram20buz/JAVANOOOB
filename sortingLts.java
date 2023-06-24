37fe7b3f26e95167036ea5c028b3de8b1b9524d7
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-24 10:59:50 -0700

    Create sortingLts.java

---
+++ sortingLts.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sortingLts {
+  public static void main(String[ ] args) {
+    ArrayList<String> animals = new ArrayList<String>();
+    animals.add("tiger");
+    animals.add("cat");
+    animals.add("snake");
+    animals.add("dog");
+
+    Collections.sort(animals);
+
+    System.out.println(animals);
+  }
+}
