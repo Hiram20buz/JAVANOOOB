d407c8a2e267592117324c165b633b873f94aec4
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-24 11:12:38 -0700

    Create iterators1.java

---
+++ iterators1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.LinkedList;
+public class iterators1 {
+    public static void main(String[ ] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        while(nums.size()<5){
+            int num = scanner.nextInt();
+            nums.add(num);
+        }
+        int sum = 0;
+                for(int s: nums){
+                        sum+= s;
+                }
+        System.out.println(sum);
+    }
+}
