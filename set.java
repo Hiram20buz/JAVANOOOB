4a40daa3e797efea930c8094ee6f375af1028bd7
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-24 10:55:36 -0700

    Create set.java

---
+++ set.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class set {
+  public static void main(String[ ] args) {
+    HashSet<String> set = new HashSet<String>();
+    set.add("A");
+    set.add("B");
+    set.add("C");
+    System.out.println(set);
+  }
+}
